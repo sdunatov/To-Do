1260d88d8cc489fe52a7eb4754e8fe1b5b5f8be9
Suzana Marija Dunatov <sdunatov@pmfst.hr>
2021-11-09 17:02:07 +0100

    Flex box

--- main.js
+++ main.js
@@ -2,20 +2,25 @@ const btnAdd = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const toDos = document.querySelector('.toDos');
 
+const saveToLocalStorage = () => {
+  localStorage.setItem('todo', toDos.textContent)
+}
 btnAdd.addEventListener('click', (e) => {
-    const todo = document.createElement('p');
-    todo.innerText = input.value;
-    toDos.appendChild(todo);
-    input.value = "";
-    todo.style.margin = '5px';
-    
-    todo.addEventListener('click', (e) => {
-        todo.style.textDecoration = 'line-through';
-        todo.style.color = '#fc6f03';
-    })
+  const todo = document.createElement('p', 'i');
+  todo.innerText = input.value;
+  toDos.appendChild(todo);
+  input.value = "";
+  todo.style.margin = '5px';
 
-    todo.addEventListener('dblclick', (e) => {
-        toDos.removeChild(todo);
-    })  
+  todo.addEventListener('click', (e) => {
+    todo.style.textDecoration = 'line-through';
+    todo.style.color = '#fc6f03';
+  })
 
-})
+  todo.addEventListener('dblclick', (e) => {
+    toDos.removeChild(todo);
+  })
+
+});
+
+btnAdd.addEventListener('click', saveToLocalStorage);
